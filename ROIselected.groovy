5fce72557298ec95fd0b8f98edf23a19e58eac59
Raktim Mondol <raktim.live@gmail.com>
2021-06-04 11:01:28 +0600

    Add files via upload

---
+++ ROIselected.groovy
@@ -0,0 +1,18 @@
+// Write the full image (only possible if it isn't too large!)
+def server = getCurrentServer()
+//writeImage(server, './full.tif')
+
+// Write the full image downsampled by a factor of 10
+//def requestFull = RegionRequest.createInstance(server, 10)
+//writeImageRegion(server, requestFull, './full_downsampled.tif')
+
+// Write the region of the image corresponding to the currently-selected object
+def roi = getSelectedROI()
+def requestROI = RegionRequest.createInstance(server.getPath(), 1, roi)
+writeImageRegion(server, requestROI, './region.tif')
+
+// Write the full image, displaying objects according to how they are currently shown in the viewer
+//def viewer = getCurrentViewer()
+//writeRenderedImage(viewer, './rendered.png')
+
+print "DONE"
