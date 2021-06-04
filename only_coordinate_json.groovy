3041b47d6cd6816f70726fd637eb4e390c99e0b2
Raktim Mondol <raktim.live@gmail.com>
2021-06-04 11:02:55 +0600

    Add files via upload

--- only_coordinate_json.groovy
+++ only_coordinate_json.groovy
@@ -1,9 +1,22 @@
 def annotations = getAnnotationObjects()
+def rois = getAnnotationObjects().collect {it.getROI()}
+
 boolean prettyPrint = true
+
 def gson = GsonTools.getInstance(prettyPrint)
 
-def path = "kfile.json"
+def path = "file_name.json"
 def file = new File(path)
-file.write(gson.toJson(annotations))
+
+
+
+
+
+
+
+file.write(gson.toJson(rois))
+
+
+
 
 print "Done!"
